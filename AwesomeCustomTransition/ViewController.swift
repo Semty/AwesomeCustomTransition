0b62181d1d77f8de0ab8c3594a2b6361e4d18126
Ruslan Timchenko <rustimchek@mail.ru>
2018-03-27 22:14:20 +0300

    Initial Commit

---
+++ AwesomeCustomTransition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AwesomeCustomTransition
+//
+//  Created by Ruslan Timchenko on 27/03/2018.
+//  Copyright © 2018 Ruslan Timchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
