18c40076e4d0a8e9b802511c1b8b073dc1878308
Kirill Solovyov <kirill_solovyov@yahoo.com>
2018-05-30 21:49:56 +0300

    AwesomeCustomTransitionPresentationController refactoring

--- AwesomeCustomTransition/Custom Transition/PresentationController.swift
+++ AwesomeCustomTransition/Custom Transition/PresentationController.swift
@@ -9,124 +9,163 @@
 import Foundation
 import UIKit
 
-// for present
-class PresentationController: UIPresentationController {
+private extension TimeInterval {
+    static let duration: TimeInterval = 0.6
+}
+
+private extension CGFloat {
+    static let blackLayerAlpha: CGFloat = 0.5
+    static let cornerRadius: CGFloat = 10
+    static let presantingSpringWithDamping: CGFloat = 0.9
+    static let presantingInitialSpringVelocity: CGFloat = 0.5
+    static let hiddenSpringWithDamping: CGFloat = 0.8
+    static let hiddenInitialSpringVelocity: CGFloat = 0.5
+    static let contentsViewYOffset: CGFloat = 20
+    static let contentsViewYCoeff: CGFloat = 0.05
+}
+
+class PresentationController: UIPresentationController, UIViewControllerAnimatedTransitioning {
     
-    var duration: TimeInterval = 0.6
-    var isPresenting = false
-    var fakeTabbar: UIView?
+    // Models
+    enum State {
+        case presenting // the cover is shown
+        case hidden // the cover is hidden
+    }
+    
+    var state: State = .hidden
+    
+    private var fakeTabbar: UIView?
     
     private var blackLayer: UIView = {
         let view = UIView()
         view.backgroundColor = .black
-        view.alpha = 0.0
+        view.alpha = 0
         return view
     }()
     
+    // MARK: - Transition Lifecycle
+    
     override func presentationTransitionWillBegin() {
-        guard let fromView = presentingViewController.view else { return }
-        guard let coordinator = presentedViewController.transitionCoordinator else { return }
-        guard let presentingVC = presentingViewController as? MainTabBarController else { return }
-        guard let contentsView = presentingVC.selectedViewController?.view else { return }
+        guard let fromView = presentingViewController.view,
+            let coordinator = presentedViewController.transitionCoordinator,
+            let presentingVC = presentingViewController as? MainTabBarController,
+            let contentsView = presentingVC.selectedViewController?.view else { return }
         
         blackLayer.frame = fromView.frame
         contentsView.addSubview(blackLayer)
         coordinator.animate(alongsideTransition: { (context) in
-            self.blackLayer.alpha = 0.5
-        }) { (_) in
-            
-        }
+            self.blackLayer.alpha = .blackLayerAlpha
+        })
     }
-}
-
-// for dismiss
-extension PresentationController {
+    
+    // For dismiss
     override func dismissalTransitionWillBegin() {
         guard let coordinator = presentedViewController.transitionCoordinator else { return }
         coordinator.animate(alongsideTransition: { (context) in
-            self.blackLayer.alpha = 0.0
+            self.blackLayer.alpha = 0
         }) { (context) in
             if !context.isCancelled {
                 self.blackLayer.removeFromSuperview()
             }
         }
     }
-}
-
-extension PresentationController: UIViewControllerAnimatedTransitioning {
+    
+    //MARK: - Private
+    
+    private func showPresentingAnimation(using transitionContext: UIViewControllerContextTransitioning) {
+        guard
+            let presented = presentedView,
+            let container = containerView,
+            let presentedVC = presentedViewController as? InfoCardViewController,
+            let presentingVC = presentingViewController as? MainTabBarController,
+            let contentsView = presentingVC.selectedViewController?.view,
+            let fakeTabbar = presentingVC.tabBar.snapshotView(afterScreenUpdates: false) else { return }
+        
+        self.fakeTabbar = fakeTabbar
+        fakeTabbar.frame = presentingVC.tabBar.frame
+        container.addSubview(presented)
+        container.addSubview(fakeTabbar)
+        presented.frame = CGRect(x: 0,
+                                 y: container.bounds.height - Constant.tabBarHeight,
+                                 width: container.bounds.width,
+                                 height: Constant.pinnedBarHeight)
+        UIView.animate(withDuration: .duration, delay: 0, usingSpringWithDamping: .presantingSpringWithDamping, initialSpringVelocity: .presantingInitialSpringVelocity, options: .curveEaseInOut, animations: {
+            contentsView.layer.cornerRadius = .cornerRadius
+            contentsView.layer.maskedCorners =
+                [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+            contentsView.clipsToBounds = true
+            contentsView.transform =
+                contentsView.transform.scaledBy(x: Constant.homeScaleX,
+                                                y: Constant.homeScaleY)
+            contentsView.transform = contentsView.transform.translatedBy(x: 0, y: .contentsViewYOffset - container.bounds.height * .contentsViewYCoeff / 2)
+            presentingVC.tabBar.frame = presentingVC.tabBar.frame.offsetBy(dx: 0, dy: presentingVC.tabBar.bounds.height)
+            fakeTabbar.frame = fakeTabbar.frame.offsetBy(dx: 0, dy: fakeTabbar.bounds.height)
+            
+            presentedVC.frameAfterPresent()
+            presented.frame = CGRect(x: 0,
+                                     y: Constant.pinnedBarInfoTopPadding,
+                                     width: container.bounds.width,
+                                     height: container.bounds.height - Constant.pinnedBarInfoTopPadding)
+            presented.layer.cornerRadius = .cornerRadius
+            presented.layer.maskedCorners =
+                [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+            presented.clipsToBounds = true
+        }, completion: { (_) in
+            transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+        })
+    }
+    
+    private func showHiddenAnimation(using transitionContext: UIViewControllerContextTransitioning) {
+        guard
+            let presented = presentedView,
+            let container = containerView,
+            let presentedVC = presentedViewController as? InfoCardViewController,
+            let presentingVC = presentingViewController as? MainTabBarController,
+            let contentsView = presentingVC.selectedViewController?.view,
+            let fakeTabbar = fakeTabbar else { return }
+        
+        UIView.animate(withDuration: .duration,
+                       delay: 0,
+                       usingSpringWithDamping: .hiddenSpringWithDamping,
+                       initialSpringVelocity: .hiddenInitialSpringVelocity,
+                       options: .curveEaseInOut,
+                       animations: {
+                        
+                        contentsView.layer.cornerRadius = 0
+                        contentsView.transform = CGAffineTransform.identity
+                        contentsView.frame = container.frame
+                        presentingVC.tabBar.frame = presentingVC.tabBar.frame.offsetBy(dx: 0, dy: -presentingVC.tabBar.bounds.height)
+                        presentingVC.pinnedBarView.isHidden = true
+                        fakeTabbar.frame = fakeTabbar.frame.offsetBy(dx: 0, dy: -fakeTabbar.bounds.height)
+                        
+                        presentedVC.frameBeforePresent()
+                        presented.layer.cornerRadius = 0
+                        presented.frame = CGRect(x: 0,
+                                                 y: container.bounds.height - Constant.tabBarHeight,
+                                                 width: container.bounds.width,
+                                                 height: Constant.pinnedBarHeight)
+        }, completion: { (_) in
+            if !transitionContext.transitionWasCancelled {
+                fakeTabbar.removeFromSuperview()
+                self.fakeTabbar = nil
+                presentingVC.pinnedBarView.isHidden = false
+            }
+            transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+        })
+    }
+    
+    //MARK: - UIViewControllerAnimatedTransitioning
+    
     func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
-        return duration
+        return .duration
     }
     
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
-        guard let presented = presentedView else { return }
-        guard let container = containerView else { return }
-        
-        guard let presentedVC = presentedViewController as? InfoCardViewController else { return }
-        guard let presentingVC = presentingViewController as? MainTabBarController else { return }
-        guard let contentsView = presentingVC.selectedViewController?.view else { return }
-        if isPresenting {
-            fakeTabbar = presentingVC.tabBar.snapshotView(afterScreenUpdates: false)
-            fakeTabbar?.frame = presentingVC.tabBar.frame
-            container.addSubview(presented)
-            container.addSubview(fakeTabbar!)
-            presented.frame = CGRect(x: 0,
-                                     y: container.bounds.height - Constant.tabBarHeight,
-                                     width: container.bounds.width,
-                                     height: Constant.pinnedBarHeight)
-            UIView.animate(withDuration: duration, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
-                contentsView.layer.cornerRadius = 10
-                contentsView.layer.maskedCorners =
-                    [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-                contentsView.clipsToBounds = true
-                contentsView.transform =
-                    contentsView.transform.scaledBy(x: Constant.homeScaleX,
-                                                     y: Constant.homeScaleY)
-                contentsView.transform = contentsView.transform.translatedBy(x: 0, y: 20 - container.bounds.height * 0.05 / 2)
-                presentingVC.tabBar.frame = presentingVC.tabBar.frame.offsetBy(dx: 0, dy: presentingVC.tabBar.bounds.height)
-                self.fakeTabbar!.frame = self.fakeTabbar!.frame.offsetBy(dx: 0, dy: self.fakeTabbar!.bounds.height)
-                
-                presentedVC.frameAfterPresent()
-                presented.frame = CGRect(x: 0,
-                                         y: Constant.pinnedBarInfoTopPadding,
-                                         width: container.bounds.width,
-                                         height: container.bounds.height - Constant.pinnedBarInfoTopPadding)
-                presented.layer.cornerRadius = 10
-                presented.layer.maskedCorners =
-                    [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-                presented.clipsToBounds = true
-            }, completion: { (_) in
-                transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
-            })
-        } else {
-            UIView.animate(withDuration: duration,
-                           delay: 0,
-                           usingSpringWithDamping: 0.8,
-                           initialSpringVelocity: 0.5,
-                           options: .curveEaseInOut,
-                           animations: {
-                            
-                contentsView.layer.cornerRadius = 0
-                contentsView.transform = CGAffineTransform.identity
-                contentsView.frame = container.frame
-                presentingVC.tabBar.frame = presentingVC.tabBar.frame.offsetBy(dx: 0, dy: -presentingVC.tabBar.bounds.height)
-                presentingVC.pinnedBarView.isHidden = true
-                self.fakeTabbar!.frame = self.fakeTabbar!.frame.offsetBy(dx: 0, dy: -self.fakeTabbar!.bounds.height)
-                
-                presentedVC.frameBeforePresent()
-                presented.layer.cornerRadius = 0
-                presented.frame = CGRect(x: 0,
-                                         y: container.bounds.height - Constant.tabBarHeight,
-                                         width: container.bounds.width,
-                                         height: Constant.pinnedBarHeight)
-            }, completion: { (_) in
-                if !transitionContext.transitionWasCancelled {
-                    self.fakeTabbar?.removeFromSuperview()
-                    self.fakeTabbar = nil
-                    presentingVC.pinnedBarView.isHidden = false
-                }
-                transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
-            })
+        switch state {
+        case .presenting:
+            showPresentingAnimation(using: transitionContext)
+        default:
+            showHiddenAnimation(using: transitionContext)
         }
     }
 }
